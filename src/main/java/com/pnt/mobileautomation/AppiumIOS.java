90c7ae186da891b6d3355775c90cb173513e5c79
alilecheabdeslam87@gmai.com <alilecheabdeslam87@gmail.com>
2021-01-05 19:31:51 -0500

    ios

---
+++ src/main/java/com/pnt/mobileautomation/AppiumIOS.java
@@ -0,0 +1,42 @@
+package com.pnt.mobileautomation;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppiumIOS {
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.4");
+        cap.setCapability(MobileCapabilityType.UDID, "R58MC41NVYE");
+
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "");     // will get the informations from the developercap.setCapability(MobileCapabilityType.APP_ACTIVITY,"");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        cap.setCapability("bundleId", "");
+
+        AppiumDriver driver = new IOSDriver(new URL("http://localhost:4723/wd/hub"), cap);
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        Thread.sleep(10000);
+
+        driver.findElement(By.xpath("//android.widget.TextView[@text='View Accounts']")).click();
+
+        Thread.sleep(10000);
+        driver.quit();
+
+
+    }
+}
+
+
